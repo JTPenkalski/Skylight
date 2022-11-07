4fbc3182a9fc532b8d532b9fdc778105de71c225
Justin Penkalski <jbonepen@gmail.com>
2022-11-07 15:50:51 -0600

    Refactored the navigation bar into its own component for increased modularity and accessibility.

--- Skylight/Skylight-WebUI/src/modules/app.module.ts
+++ Skylight/Skylight-WebUI/src/modules/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,17 +13,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from '../components/app/app.component';
 import { FormLocationComponent } from '../components/forms/location/form-location.component';
+import { NavigationBarComponent } from '../components/navigation-bar/navigation-bar.component'
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [
     AppComponent,
-    FormLocationComponent
+    FormLocationComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
+    MatAutocompleteModule,
     MatButtonModule,
     MatFormFieldModule,
     MatIconModule,
