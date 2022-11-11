92189762b4057a0193cdab0cb5f1aec9055008ee
Justin Penkalski <jbonepen@gmail.com>
2022-11-11 17:29:35 -0600

    Temporary changes using multiple Owned Entity types.

--- Skylight/Skylight-API/Models/RiskCategory.cs
+++ Skylight/Skylight-API/Models/RiskCategory.cs
@@ -1,44 +1,82 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace Skylight.Models
 {
     public class RiskCategory
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Code { get; set; }
         public string Category { get; set; }
         public string Details { get; set; }
         public string Summary { get; set; }
-        public ICollection<RiskCategoryOutlookProbability> TornadoRisk { get; set; }
-        public ICollection<RiskCategoryOutlookProbability> WindRisk { get; set; }
-        public ICollection<RiskCategoryOutlookProbability> HailRisk { get; set; }
+        public RiskCategoryOutlookProbabilityTables RiskProbabilities { get; set; }
 
         public RiskCategory(
-            int id,
             string code,
             string category,
             string details,
             string summary
-        ) : this(id, code, category, details, summary, new List<RiskCategoryOutlookProbability>(), new List<RiskCategoryOutlookProbability>(), new List<RiskCategoryOutlookProbability>()) { }
+        ) : this(code, category, details, summary, new()) { }
 
         public RiskCategory(
-            int id, 
             string code, 
             string category, 
             string details, 
-            string summary, 
-            ICollection<RiskCategoryOutlookProbability> tornadoRisk, 
-            ICollection<RiskCategoryOutlookProbability> windRisk,
-            ICollection<RiskCategoryOutlookProbability> hailRisk
+            string summary,
+            RiskCategoryOutlookProbabilityTables riskProbabilities
         )
         {
-            Id = id;
             Code = code;
             Category = category;
             Details = details;
             Summary = summary;
+            RiskProbabilities = riskProbabilities;
+        }
+    }
+
+    [Owned]
+    public class RiskCategoryOutlookProbabilityTables
+    {
+        public RiskCategoryOutlookProbabilityTable DayOne { get; init; }
+        public RiskCategoryOutlookProbabilityTable DayTwo { get; init; }
+        public RiskCategoryOutlookProbabilityTable DayThree { get; init; }
+        public RiskCategory RiskCategory { get; init; } = null!;
+
+        public RiskCategoryOutlookProbabilityTables() : this(new(), new(), new()) { }
+
+        public RiskCategoryOutlookProbabilityTables(
+            RiskCategoryOutlookProbabilityTable dayOne,
+            RiskCategoryOutlookProbabilityTable dayTwo,
+            RiskCategoryOutlookProbabilityTable dayThree
+        )
+        {
+            DayOne = dayOne;
+            DayTwo = dayTwo;
+            DayThree = dayThree;
+        }
+    }
+
+    [Owned]
+    public class RiskCategoryOutlookProbabilityTable
+    {
+        public ICollection<RiskCategoryOutlookProbability> TornadoRisk { get; init; }
+        public ICollection<RiskCategoryOutlookProbability> WindRisk { get; init; }
+        public ICollection<RiskCategoryOutlookProbability> HailRisk { get; init; }
+        public RiskCategoryOutlookProbabilityTables ProbabilityTables { get; init; } = null!;
+
+        public RiskCategoryOutlookProbabilityTable() : this(RiskCategoryOutlookProbability.Unused, RiskCategoryOutlookProbability.Unused, RiskCategoryOutlookProbability.Unused) { }
+
+        public RiskCategoryOutlookProbabilityTable(
+            ICollection<RiskCategoryOutlookProbability> tornadoRisk,
+            ICollection<RiskCategoryOutlookProbability> windRisk,
+            ICollection<RiskCategoryOutlookProbability> hailRisk
+        )
+        {
             TornadoRisk = tornadoRisk;
             WindRisk = windRisk;
             HailRisk = hailRisk;
@@ -48,23 +86,25 @@ namespace Skylight.Models
     [Owned]
     public class RiskCategoryOutlookProbability
     {
-        private static ICollection<RiskCategoryOutlookProbability> _unused = new List<RiskCategoryOutlookProbability> { new(0f, false) };
+        private static readonly ICollection<RiskCategoryOutlookProbability> _unused = new List<RiskCategoryOutlookProbability> { new(0, false) };
         public static ICollection<RiskCategoryOutlookProbability> Unused => _unused;
 
-        public float Chance { get; init; }
+        [Key] public int Id { get; init; }
+        public int Chance { get; init; }
         public bool SignificantSevere { get; init; }
+        [ForeignKey("RiskCategoryId")] public RiskCategory RiskCategory { get; init; } = null!;
 
-        public RiskCategoryOutlookProbability(float chance, bool significantSevere)
+        public RiskCategoryOutlookProbability(int chance, bool significantSevere)
         {
-            Chance = chance;
+            Chance = Math.Clamp(chance, 0, 100);
             SignificantSevere = significantSevere;
         }
 
-        public static ICollection<RiskCategoryOutlookProbability> Build(params (float chance, bool sigSevere)[] probabilities)
+        public static ICollection<RiskCategoryOutlookProbability> Build(params (int chance, bool sigSevere)[] probabilities)
         {
-            return (probabilities is null || probabilities.Length == 0)
-                ? Unused
-                : new List<RiskCategoryOutlookProbability>(probabilities.Select(p => new RiskCategoryOutlookProbability(p.chance, p.sigSevere)));
+            return probabilities.Any()
+                ? new List<RiskCategoryOutlookProbability>(probabilities.Select(p => new RiskCategoryOutlookProbability(p.chance, p.sigSevere)))
+                : Unused;
         }
     }
 }
