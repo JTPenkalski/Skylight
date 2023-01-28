3e76c61cbc26b9e571bd1b47b3032820b2978f44
Justin Penkalski <jbonepen@gmail.com>
2023-01-28 15:29:26 -0600

    Setting up new infrastructure for revised Dynamic Forms flow. App most certainly broken.

---
+++ Skylight/Skylight-Infrastructure/Services/FormsConverter.cs
@@ -0,0 +1,14 @@
+﻿using Skylight_Infrastructure.Interfaces;
+
+namespace Skylight_Infrastructure.Services
+{
+    public class FormsConverter : IFormsConverter
+    {
+        private const string ATTRIBUTE_KEY = "attributes";
+
+        public string ConvertToJson(string xml)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
