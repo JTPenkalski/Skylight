d976e630ac16d0863f9c674d4df7045f1cd4fe87
Justin Penkalski <jbonepen@gmail.com>
2024-05-06 17:25:40 -0500

    Deleted old projects.

--- Skylight/Skylight-Core/Forms/FormGuide.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Skylight.Forms
-{
-    /// <summary>
-    /// Base container for metadata which helps determine, or guide,
-    /// how a collection of <see cref="FormControlGuide{T}"/> instances should operate on the UI.
-    /// </summary>
-    public abstract class FormGuide { }
-}
--- Skylight/Skylight-WebUI/src/core/services/weather-service-interface.service.ts
+++
@@ -1,6 +0,0 @@
-import { Weather } from 'presentation/models';
-import { IService } from './index';
-
-export interface IWeatherService extends IService<Weather> {
-  
-}
