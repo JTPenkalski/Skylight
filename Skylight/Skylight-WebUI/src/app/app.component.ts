403d79695af3365bc915c7f51d1942d2f05c6051
Justin Penkalski <jbonepen@gmail.com>
2024-05-06 20:51:16 -0500

    Got a demo page displaying again.

--- Skylight/Skylight-WebUI/src/app/app.component.ts
+++ Skylight/Skylight-WebUI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NbButtonComponent, NbButtonModule, NbLayoutComponent, NbLayoutModule, NbSidebarComponent, NbSidebarModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NbLayoutModule, NbSidebarModule, NbButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Skylight/Skylight-WebUI/src/app/app.config.ts
+++ Skylight/Skylight-WebUI/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { NbButtonModule, NbLayoutModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      NbThemeModule.forRoot({
+        name: 'dark'
+      }),
+      NbSidebarModule.forRoot()
+    )
+  ]
 };
