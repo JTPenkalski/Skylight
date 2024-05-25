877b7d22ead07d7440ef1c915a8e0ece914c3904
Justin Penkalski <jbonepen@gmail.com>
2024-05-25 00:43:35 -0500

    Quieted JS unit tests for now, maybe.

--- Skylight/Skylight-WebUI/src/shared/directives/darken/darken.directive.spec.ts
+++ Skylight/Skylight-WebUI/src/shared/directives/darken/darken.directive.spec.ts
@@ -1,8 +1,9 @@
+import { ElementRef } from '@angular/core';
 import { DarkenDirective } from './darken.directive';
 
 describe('DarkenDirective', () => {
   it('should create an instance', () => {
-    const directive = new DarkenDirective();
+    const directive = new DarkenDirective(new ElementRef(null));
     expect(directive).toBeTruthy();
   });
 });
--- Skylight/Skylight-WebUI/src/shared/directives/no-select/no-select.directive.spec.ts
+++ Skylight/Skylight-WebUI/src/shared/directives/no-select/no-select.directive.spec.ts
@@ -2,7 +2,7 @@ import { NoSelectDirective } from './no-select.directive';
 
 describe('NoSelectDirective', () => {
   it('should create an instance', () => {
-    const directive = new NoSelectDirective();
+    const directive = new NoSelectDirective(null!);
     expect(directive).toBeTruthy();
   });
 });
