81cc783a75267611c87c2cf0d53fd1c22760a3a8
Justin Penkalski <jbonepen@gmail.com>
2025-04-06 20:25:10 -0500

    Fixed spelling error.

--- src/Skylight.AppHost/Program.cs
+++ src/Skylight.AppHost/Program.cs
@@ -3,13 +3,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 var username = builder.AddParameter("username", secret: true);
 var password = builder.AddParameter("password", secret: true);
 
-var postgress = builder
-	.AddPostgres("skylight-postgress", username, password)
-	.AddDatabase("skylight-postgress-db");
+var postgres = builder
+	.AddPostgres("skylight-postgres", username, password)
+	.AddDatabase("skylight-postgres-db");
 
 var api = builder
 	.AddProject<Projects.Skylight_API>("skylight-api")
-	.WithReference(postgress)
-	.WaitFor(postgress);
+	.WithReference(postgres)
+	.WaitFor(postgres);
 
 await builder.Build().RunAsync();
